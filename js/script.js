99e8f1bba0ea07cdf65df304374d879543f64914
Jamesmbaya <chinkupozee@gmail.com>
2025-01-18 07:31:56 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+const form = document.querySelector('form');
+
+function sendEmail(){
+    sendEmail.send({
+    Host : "firebase.google.com",
+    Username : "portfolioproject7@gmail.com",
+    Password : "9qMQs1sjCwTqYT37QL2NWk8wgv53",
+}).then (
+    message => alert(message)
+);
+
+}
+
