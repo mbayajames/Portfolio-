7f897d8e7cc8ba06211ee5aef0dc3e35b907534f
Jamesmbaya <chinkupozee@gmail.com>
2025-01-31 02:24:11 -0800

    modify

--- js/script.js
+++ js/script.js
@@ -19,6 +19,27 @@ function sendEmail(){
 
 }
 
+var icon = document.getElementById("icon");
+
+            if (localStorage.getItem("theme") === "dark") {
+                document.body.classList.add("dark-theme");
+                icon.src = "images/sun.png";
+            } else {
+                document.body.classList.remove("dark-theme");
+                icon.src = "images/moon.png";
+            }
+
+                icon.onclick = function () {
+                document.body.classList.toggle("dark-theme");
+            if (document.body.classList.contains("dark-theme")) {
+                icon.src = "images/sun.png";
+                localStorage.setItem("theme", "dark");
+            } else {
+                 icon.src = "images/moon.png";
+                 localStorage.setItem("theme", "light");
+              }
+            };
+
 
 
 
